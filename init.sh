257d891e08dddbc153d2d9599c2b5ff2a9cf2707
John Doe <you@example.com>
2020-11-27 11:52:23 -0400

    Updates for running strat sync detached

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 ssh-add - <<< "${SYNCBOT_SSH_KEY}"
 
 python3 initialize_key_files.py
-python3 sync_strategies.py
 
-#!/bin/bash
+tmux new-session -d -s sync_strategies -n hbstrat
+tmux send-keys -t sync_strategies:hbstrat "python3 sync_strategies.py &" Enter
+
+sleep 10
+
 tmux new-session -d -s hummingbot -n hbstrat
 tmux send-keys -t hummingbot:hbstrat "python3 bin/hummingbot_quickstart.py" Enter
 
@@ -11,11 +15,7 @@ sleep 5
 
 tmux send-keys -t hummingbot:hbstrat "import ${CONFIG_FILE_NAME}" Enter
 
-#GET PID by parsing?? or Force PID
-#TAIL PID
 tail -f /dev/null
 
 ###### To attach to Hummingbot Session run
 ##   tmux attach -t hummingbot:hbstrat
-
-
